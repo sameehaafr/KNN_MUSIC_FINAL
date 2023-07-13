7de9047a315f6b620618edea5b0ba429f0bbada0
Sameeha Afrulbasha <sameehaafr@gmail.com>
2023-07-12 23:40:51 -0400

    ch

--- knn_sim.py
+++ knn_sim.py
@@ -228,29 +228,28 @@ def get_audio_features(song_name, artist):
             return numerical_features
     return None
 
-try:
-  # Create a Streamlit app
-    st.title('Audio Features Lookup')
+# Create a Streamlit app
+st.title('Audio Features Lookup')
+st.write('Enter a song name and artist to get the audio features.')
+
+# Get user input
+song_name = st.text_input('Song Name')
+artist = st.text_input('Artist')
+audio_feats = None
+
+if not song_name and not artist:
     st.write('Enter a song name and artist to get the audio features.')
+elif song_name and artist:
+    # Get audio features
+    audio_feats = []
+    audio_feats = get_audio_features(song_name, artist)
 
-    # Get user input
-    song_name = st.text_input('Song Name')
-    artist = st.text_input('Artist')
-    audio_feats = None
-    if song_name and artist:
-        # Get audio features
-        audio_feats = []
-        audio_feats = get_audio_features(song_name, artist)
-
-        if audio_feats:
-            st.write('Audio Features:')
-            audio_table = pd.DataFrame.from_dict(audio_feats, orient='index', columns=['Value'])
-            st.table(audio_table)
-        else:
-            st.write('No audio features found for the given song and artist.')
-except:
-  # Prevent the error from propagating into your Streamlit app.
-  pass
+    if audio_feats:
+        st.write('Audio Features:')
+        audio_table = pd.DataFrame.from_dict(audio_feats, orient='index', columns=['Value'])
+        st.table(audio_table)
+    else:
+        st.write('No audio features found for the given song and artist.')
 
 def knn(k, X, y, audio_feats):
     scaler = StandardScaler()
