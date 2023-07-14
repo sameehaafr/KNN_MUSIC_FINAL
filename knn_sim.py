000aa6c19092fa2c6149b56de73ac0ef4df0651d
Sameeha Afrulbasha <sameehaafr@gmail.com>
2023-07-14 18:43:53 -0400

    changes

--- knn_sim.py
+++ knn_sim.py
@@ -220,10 +220,10 @@ st.caption("By following this code and understanding the described steps, one ca
 # -------------ABOUT ME------------ #
 st.header("Shameless Self-Promotion")
 st.caption("If you liked this project, checkout my other projects and some of my other media accounts below!")
-st.button("Website", key="website", on_click=lambda: wb.open_new_tab("https://sameehaafr.github.io/sameehaafr/"), use_container_width=True)
-st.button("GitHub", key="github", on_click=lambda: wb.open_new_tab("https://github.com/sameehaafr"), use_container_width=True)
-st.button("LinkedIn", key="linkedin", on_click=lambda: wb.open_new_tab("https://www.linkedin.com/in/sameeha-afrulbasha/"), use_container_width=True)
-st.button("Medium", key="medium", on_click=lambda: wb.open_new_tab("https://sameehaafr.medium.com/"), use_container_width=True)
+st.caption("Website: https://sameehaafr.github.io/sameehaafr/")
+st.caption("GitHub: https://github.com/sameehaafr")
+st.caption("LinkedIn: https://www.linkedin.com/in/sameeha-afrulbasha/")
+st.caption("Medium: https://sameehaafr.medium.com/")
 
 #st.caption("[Website](https://sameehaafr.github.io/sameehaafr/) \n\n\n [GitHub](https://github.com/sameehaafr) \n\n\n [LinkedIn]() \n\n\n [Medium](https://sameehaafr.medium.com/)")
 
